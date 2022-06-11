c9d72f0a0cdcf35836c66b6a24bae275f384ec9c
White-prince <tiltedfear@gmail.com>
2022-06-12 02:22:54 +0300

    edit info

--- main.py
+++ main.py
@@ -22,8 +22,14 @@ print("Аниме на 2 странице")
 for el in html2.select(".kino-item > .kino-title"):
     content = el.select('h2')
     print(content[0].text)
+    while el in html.find_all(".kino-inner > .kino-text"):
+        info = el.select(".kino-desc")
+        print(info[0].text)
 
 print("Аниме на 3 странице")
 for el in html3.select(".kino-item > .kino-title"):
     content = el.select('h2')
     print(content[0].text)
+    while el in html.find_all(".kino-inner > .kino-text"):
+        info = el.select(".kino-desc")
+        print(info[0].text)
